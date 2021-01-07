11d703519037c78b4159edf1f6ab946261de610c
Dennis-Talkington <59609876+Dennis-Talkington@users.noreply.github.com>
2021-01-07 09:23:24 -0600

    Added the program files

---
+++ color.js
@@ -0,0 +1,28 @@
+/* Solve the following pb and color accordingly
+The array is arranged 0 through 9 so replace the appropriate one. */
+
+var col = [
+  '#FFF', // 0 Don't touch this one
+  '#FFF', // 1
+  '#FFF', // 2
+  '#FFF', // 3
+  '#FFF', // 4
+  '#FFF', // 5
+  '#FFF', // 6
+  '#FFF', // 7
+  '#FFF', // 8
+  '#FFF', // 9
+]
+
+
+/* Spot |      Problem          |  Answer 1                     |   Answer 2   
+     1  |  (3, 2)  and  (4, 9)  | y = 7x - 19         '#F00'    | y = 7x + 21     '#DA9'
+     2  |  (-1, 0)  and (3, 2)  | y = (1/2)x + (1/2)  '#DA9'    | y = 2x - 1      '#00F'
+     3  |  (-2, 8) and (0, -6)  | y = -7x - 6         '#8B4513' | y = 7x + 6      '#000'
+     4  |  (-1, 3) and (-2, 7)  | y = -4x - 1         '#00F'    | y = 4x + 8      '#8B4513'
+     5  | (-5, 3) and (-2, -3)  | y = (1/2)x - 5      '#8B4513' | y = -2x - 7     '#F00'
+     6  | (-3, 2) and (-2, -1)  | y = (1/3)x - 3      '#000'    | y = -3x - 7     '#DA9'
+     7  |(-3, -11) and (-8, -1) | y = 2x + 15         '#DA9'    | y = -2x - 17    '#8B4513'   
+     8  |  (3, -9) and (0, 0)   | y = -3x             '#00F'    | y = -3x + 18    '#F00'   
+     9  |  (8, -1) and (0, 3)   | y = -2x + 15        '#00F'    | y = -(1/2)x + 3 '#000'
+*/
